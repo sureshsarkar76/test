016786b0c9db978a34cb7185aa653ba657ceb215
sureshsarkar76 <sureshsarkar76@gmail.com>
2017-09-25 17:28:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
  
-  response.json({ "speech": "response", "displayText": "response" });
+  response.json({ "speech": "response", "displayText": "response1" });
 })
 
 app.listen(app.get('port'), function() {
