b8493d39ac306807fc3b7756661e8b59ef15615c
sureshsarkar76 <sureshsarkar76@gmail.com>
2017-09-25 15:01:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World-edited')
+  response.send('Hello World-edited-now')
 })
 
 app.listen(app.get('port'), function() {
