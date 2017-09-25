bda3d18420f4455fbcc7cecbc84b286df30a2bc0
sureshsarkar76 <sureshsarkar76@gmail.com>
2017-09-25 15:15:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+  response = "This is a sample response from your webhook!";
 
  
   response.json({ "speech": response, "displayText": response });
